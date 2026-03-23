48c41ae765e46b064eceaea7ba1a5956d653e63e
SSOL <msseock@gmail.com>
2026-03-23 17:25:15 +0900

    프로젝트 파일 생성
    - CoreData + CloudKit 기반
    - 스토리보드 없는 코드베이스 UI 설정

---
+++ GoTato/GoTato/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  GoTato
+//
+//  Created by 석민솔 on 3/23/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
