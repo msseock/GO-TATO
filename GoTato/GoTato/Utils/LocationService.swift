c3e97e6ccc271090ef2237e4d6b0874abb031007
SSOL <msseock@gmail.com>
2026-04-02 00:49:51 +0900

    LocationService 지오펜싱 및 Always 권한 지원 추가

--- GoTato/GoTato/Utils/LocationService.swift
+++ GoTato/GoTato/Utils/LocationService.swift
@@ -13,6 +13,7 @@ final class LocationService: NSObject {
     private let manager = CLLocationManager()
     let authorizationStatus: BehaviorRelay<CLAuthorizationStatus>
     let currentLocation = PublishRelay<CLLocation>()
+    let didEnterRegion = PublishRelay<CLRegion>()
 
     private override init() {
         authorizationStatus = BehaviorRelay(value: manager.authorizationStatus)
@@ -20,7 +21,10 @@ final class LocationService: NSObject {
         manager.delegate = self
         manager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
         manager.distanceFilter = 10
+        manager.allowsBackgroundLocationUpdates = true
     }
+
+    // MARK: - Authorization
 
     func checkLocationAuthorization() {
         if !CLLocationManager.locationServicesEnabled() {
@@ -39,12 +43,49 @@ final class LocationService: NSObject {
         }
     }
 
+    func requestAlwaysAuthorization() {
+        manager.requestAlwaysAuthorization()
+    }
+
+    // MARK: - Location Updates (포그라운드 폴링용)
+
     func startUpdatingLocation() {
         manager.startUpdatingLocation()
     }
 
     func stopUpdatingLocation() {
         manager.stopUpdatingLocation()
+    }
+
+    // MARK: - Region Monitoring (지오펜싱)
+
+    func startMonitoringRegion(center: CLLocationCoordinate2D, radius: CLLocationDistance, identifier: String) {
+        guard CLLocationManager.isMonitoringAvailable(for: CLCircularRegion.self) else { return }
+        guard authorizationStatus.value == .authorizedAlways else { return }
+
+        let region = CLCircularRegion(center: center, radius: radius, identifier: identifier)
+        region.notifyOnEntry = true
+        region.notifyOnExit = false
+        manager.startMonitoring(for: region)
+    }
+
+    func stopMonitoringRegion(identifier: String) {
+        for region in manager.monitoredRegions {
+            if region.identifier == identifier {
+                manager.stopMonitoring(for: region)
+                break
+            }
+        }
+    }
+
+    var monitoredRegionIdentifiers: Set<String> {
+        Set(manager.monitoredRegions.map(\.identifier))
+    }
+
+    func stopMonitoringAllRegions() {
+        for region in manager.monitoredRegions {
+            manager.stopMonitoring(for: region)
+        }
     }
 }
 
@@ -57,6 +98,12 @@ extension LocationService: CLLocationManagerDelegate {
         guard let location = locations.last else { return }
         currentLocation.accept(location)
     }
+
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        didEnterRegion.accept(region)
+    }
+
+    func locationManager(_ manager: CLLocationManager, monitoringDidFailFor region: CLRegion?, withError error: Error) {}
 
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {}
 }
