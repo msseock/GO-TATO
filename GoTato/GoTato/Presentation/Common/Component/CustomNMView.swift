667ee86d1e40dab77636dd13602493d551f033fd
SSOL <msseock@gmail.com>
2026-04-03 15:21:27 +0900

    네이버 지도 마커의 기준점(Anchor)을 중앙으로 변경

--- GoTato/GoTato/Presentation/Common/Component/CustomNMView.swift
+++ GoTato/GoTato/Presentation/Common/Component/CustomNMView.swift
@@ -16,6 +16,7 @@ class CustomNMView: UIView {
     private var cameraBounds: NMGLatLngBounds?
     private var singleCoord: NMGLatLng?
     private var didMoveCamera = false
+    private var marker: NMFMarker?
 
     init(currentCoord: NMGLatLng, destinationCoord: NMGLatLng) {
         let sw = NMGLatLng(
@@ -44,9 +45,11 @@ class CustomNMView: UIView {
 
         let marker2 = NMFMarker(position: destinationCoord)
         marker2.iconImage = NMFOverlayImage(name: "destinationMark")
+        marker2.anchor = CGPoint(x: 0.5, y: 0.5)
         marker2.width = 25
         marker2.height = 25
         marker2.mapView = mapView
+        self.marker = marker2
     }
     
     init(mapx: Int, mapy: Int) {
@@ -65,9 +68,49 @@ class CustomNMView: UIView {
         
         let marker = NMFMarker(position: coord)
         marker.iconImage = NMFOverlayImage(name: "destinationMark")
+        marker.anchor = CGPoint(x: 0.5, y: 0.5)
         marker.width = 25
         marker.height = 25
         marker.mapView = mapView
+        self.marker = marker
+    }
+
+    init(coord: NMGLatLng, isInteractive: Bool = false) {
+        self.cameraBounds = nil
+        self.singleCoord = coord
+        super.init(frame: .zero)
+
+        mapView.isScrollGestureEnabled = isInteractive
+        mapView.isZoomGestureEnabled = isInteractive
+        mapView.isUserInteractionEnabled = isInteractive
+        addSubview(mapView)
+        mapView.snp.makeConstraints { $0.edges.equalToSuperview() }
+
+        let marker = NMFMarker(position: coord)
+        marker.iconImage = NMFOverlayImage(name: "destinationMark")
+        marker.anchor = CGPoint(x: 0.5, y: 0.5)
+        marker.width = 25
+        marker.height = 25
+        marker.mapView = mapView
+        self.marker = marker
+    }
+
+    func updateCoord(coord: NMGLatLng) {
+        self.singleCoord = coord
+        if marker == nil {
+            let newMarker = NMFMarker(position: coord)
+            newMarker.iconImage = NMFOverlayImage(name: "destinationMark")
+            newMarker.anchor = CGPoint(x: 0.5, y: 0.5)
+            newMarker.width = 25
+            newMarker.height = 25
+            self.marker = newMarker
+        }
+        marker?.position = coord
+        marker?.mapView = mapView
+        
+        let update = NMFCameraUpdate(scrollTo: coord)
+        update.animation = .easeIn
+        mapView.moveCamera(update)
     }
 
     override func layoutSubviews() {
